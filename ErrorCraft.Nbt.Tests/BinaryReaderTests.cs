14ad4e15872f5f8dc3cec48e25a298e855aa7a6b
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-05-21 17:12:36 +0200

    Add unit tests for reading unsigned number methods in BinaryReader

--- ErrorCraft.Nbt.Tests/BinaryReaderTests.cs
+++ ErrorCraft.Nbt.Tests/BinaryReaderTests.cs
@@ -26,6 +26,27 @@ namespace ErrorCraft.Nbt.Tests {
         }
 
         [TestMethod]
+        public void ReadUnsignedByte_ReadsCorrectValue() {
+            byte[] bytes = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            byte result = binaryReader.ReadUnsignedByte();
+            Assert.AreEqual<byte>(128, result);
+        }
+
+        [TestMethod]
+        public void ReadUnsignedByte_ThrowsException_BecauseEndOfStreamWasReached() {
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+            byte[] bytes = new byte[0];
+#pragma warning restore CA1825 // Avoid zero-length array allocations
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedByte());
+        }
+
+        [TestMethod]
         public void ReadShort_ReadsCorrectValue() {
             byte[] bytes = new byte[] { 0x80, 0x00 };
             using MemoryStream memoryStream = new MemoryStream(bytes);
@@ -42,6 +63,25 @@ namespace ErrorCraft.Nbt.Tests {
             using BinaryReader binaryReader = new BinaryReader(memoryStream);
 
             Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadShort());
+        }
+
+        [TestMethod]
+        public void ReadUnsignedShort_ReadsCorrectValue() {
+            byte[] bytes = new byte[] { 0x80, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            ushort result = binaryReader.ReadUnsignedShort();
+            Assert.AreEqual<ushort>(32768, result);
+        }
+
+        [TestMethod]
+        public void ReadUnsignedShort_ThrowsException_BecauseEndOfStreamWasReached() {
+            byte[] bytes = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedShort());
         }
 
         [TestMethod]
@@ -64,6 +104,25 @@ namespace ErrorCraft.Nbt.Tests {
         }
 
         [TestMethod]
+        public void ReadUnsignedInt_ReadsCorrectValue() {
+            byte[] bytes = new byte[] { 0x80, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            uint result = binaryReader.ReadUnsignedInt();
+            Assert.AreEqual(2147483648, result);
+        }
+
+        [TestMethod]
+        public void ReadUnsignedInt_ThrowsException_BecauseEndOfStreamWasReached() {
+            byte[] bytes = new byte[] { 0x80, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedInt());
+        }
+
+        [TestMethod]
         public void ReadLong_ReadsCorrectValue() {
             byte[] bytes = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
             using MemoryStream memoryStream = new MemoryStream(bytes);
@@ -80,6 +139,25 @@ namespace ErrorCraft.Nbt.Tests {
             using BinaryReader binaryReader = new BinaryReader(memoryStream);
 
             Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadLong());
+        }
+
+        [TestMethod]
+        public void ReadUnsignedLong_ReadsCorrectValue() {
+            byte[] bytes = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            ulong result = binaryReader.ReadUnsignedLong();
+            Assert.AreEqual(9223372036854775808L, result);
+        }
+
+        [TestMethod]
+        public void ReadUnsignedLong_ThrowsException_BecauseEndOfStreamWasReached() {
+            byte[] bytes = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedLong());
         }
     }
 }
