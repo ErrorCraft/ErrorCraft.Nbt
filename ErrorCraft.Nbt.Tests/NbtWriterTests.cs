02ef7655938b26ba84ca1fed3d32b7151c2d5082
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-06-13 17:45:50 +0200

    Add basic NBT writer

---
+++ ErrorCraft.Nbt.Tests/NbtWriterTests.cs
@@ -0,0 +1,28 @@
+﻿using ErrorCraft.Nbt.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ErrorCraft.Nbt.Tests {
+    [TestClass]
+    public class NbtWriterTests {
+        [TestMethod]
+        public void Write_WritesCorrectValue() {
+            NbtRoot nbtRoot = new NbtRoot("foo", new ByteTag(0));
+            using MemoryStream memoryStream = new MemoryStream();
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream);
+
+            nbtWriter.Write(nbtRoot);
+            byte[] bytes = memoryStream.ToArray();
+            CollectionAssert.AreEqual(new byte[] { 0x01, 0x00, 0x03, 0x66, 0x6F, 0x6F, 0x00 }, bytes);
+        }
+
+        [TestMethod]
+        public void Write_ThrowsException_BecauseNbtRootIsNull() {
+            using MemoryStream memoryStream = new MemoryStream();
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream);
+
+            Assert.ThrowsException<ArgumentNullException>(() => nbtWriter.Write(null));
+        }
+    }
+}
---
+++ ErrorCraft.Nbt/NbtWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ErrorCraft.Nbt {
+    public class NbtWriter : IDisposable {
+        private readonly BinaryWriter Writer;
+
+        public NbtWriter(Stream stream) {
+            Writer = new BinaryWriter(stream);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Write(NbtRoot value) {
+            if (value == null) {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            Writer.WriteTagType(value.Data.GetTagType());
+            Writer.WriteString(value.RootName);
+            value.Data.Write(Writer);
+        }
+
+        private void Dispose(bool disposing) {
+            if (disposing) {
+                Writer.Dispose();
+            }
+        }
+    }
+}
