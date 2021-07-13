2ba17aefcaafb8a9ec622300810c3ca4306c9336
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-07-13 21:34:45 +0200

    Document INumberTag, ICollectionTag and ICollectionTag<T> with XML comments

--- ErrorCraft.Nbt/Tags/ICollectionTag.cs
+++ ErrorCraft.Nbt/Tags/ICollectionTag.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 
 namespace ErrorCraft.Nbt.Tags {
+    /// <summary>
+    /// Represents the interface for all NBT collection types.
+    /// </summary>
     public interface ICollectionTag : ITag, IEnumerable {
+        /// <summary>
+        /// Gets the number of items in this NBT collection.
+        /// </summary>
         int Count { get; }
+
+        /// <summary>
+        /// Gets the tag at the specified index.
+        /// </summary>
+        /// <param name="index">The index of the item to get.</param>
+        /// <returns>The item at the specified index.</returns>
         ITag this[int index] { get; }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="ICollectionTag"/>.
+        /// </summary>
+        /// <returns>An enumerator that iterates through the <see cref="ICollectionTag"/>.</returns>
         new IEnumerator<ITag> GetEnumerator();
     }
 
+    /// <summary>
+    /// Represents the generic interface for all NBT collection types.
+    /// </summary>
+    /// <typeparam name="T">The type of the items in the collection.</typeparam>
     public interface ICollectionTag<T> : ICollection<T>, ICollectionTag {
+        /// <summary>
+        /// Gets or sets the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index of the item to get.</param>
+        /// <returns>The item at the specified index.</returns>
         new T this[int index] { get; set; }
     }
 }
--- ErrorCraft.Nbt/Tags/INumberTag.cs
+++ ErrorCraft.Nbt/Tags/INumberTag.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
 namespace ErrorCraft.Nbt.Tags {
+    /// <summary>
+    /// Represents the interface for all numeric NBT types.
+    /// </summary>
     public interface INumberTag : ITag, IEquatable<INumberTag> {
+        /// <summary>
+        /// Gets the value of this number as a signed byte.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a signed byte.</returns>
         sbyte GetAsByte();
+
+        /// <summary>
+        /// Gets the value of this number as a signed 16-bit integer.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a signed short.</returns>
         short GetAsShort();
+
+        /// <summary>
+        /// Gets the value of this number as a signed 32-bit integer.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a signed integer.</returns>
         int GetAsInt();
+
+        /// <summary>
+        /// Gets the value of this number as a signed 64-bit integer.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a signed long.</returns>
         long GetAsLong();
+
+        /// <summary>
+        /// Gets the value of this number as an IEEE-754 single-precision floating point number.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a float.</returns>
         float GetAsFloat();
+
+        /// <summary>
+        /// Gets the value of this number as an IEEE-754 double-precision floating point number.
+        /// </summary>
+        /// <returns>The numeric value represented by this object after converting it to a double.</returns>
         double GetAsDouble();
     }
 }
