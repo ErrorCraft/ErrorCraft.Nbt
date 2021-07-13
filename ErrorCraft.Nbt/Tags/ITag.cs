86992278e7cb13425b76ac44b021306e7ed8a444
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-07-13 15:09:51 +0200

    Start documenting everything with XML comments

--- ErrorCraft.Nbt/Tags/ITag.cs
+++ ErrorCraft.Nbt/Tags/ITag.cs
@@ -1,7 +1,23 @@
 ﻿namespace ErrorCraft.Nbt.Tags {
+    /// <summary>
+    /// Represents the base interface for all NBT types.
+    /// </summary>
     public interface ITag {
+        /// <summary>
+        /// Gets the <see cref="TagType"/> associated with this tag.
+        /// </summary>
         TagType GetTagType();
+
+        /// <summary>
+        /// Reads the contents of the <see cref="BinaryReader"/> into this tag.
+        /// </summary>
+        /// <param name="binaryReader">The reader to read bytes from.</param>
         void Read(BinaryReader binaryReader);
+
+        /// <summary>
+        /// Writes the contents of this tag into the <see cref="BinaryWriter"/>.
+        /// </summary>
+        /// <param name="binaryWriter">The writer to write bytes to.</param>
         void Write(BinaryWriter binaryWriter);
     }
 }
