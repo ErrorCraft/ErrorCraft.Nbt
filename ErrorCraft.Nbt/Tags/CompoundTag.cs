5a1d79f13062bf0720872bb79400cfa175f099ad
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-07-18 17:26:28 +0200

    Document CompoundTag with XML comments

--- ErrorCraft.Nbt/Tags/CompoundTag.cs
+++ ErrorCraft.Nbt/Tags/CompoundTag.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 
 namespace ErrorCraft.Nbt.Tags {
+    /// <summary>
+    /// A tag containing a collection of named tags.
+    /// </summary>
     public class CompoundTag : ITag, IEnumerable<KeyValuePair<string, ITag>> {
         private Dictionary<string, ITag> Data;
 
+        /// <summary>
+        /// Gets the number of items in this NBT compound.
+        /// </summary>
         public int Count { get { return Data.Count; } }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="CompoundTag"/> class with an empty collection of tags.
+        /// </summary>
         public CompoundTag() : this(new Dictionary<string, ITag>()) { }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="CompoundTag"/> class with the specified value.
+        /// </summary>
+        /// <param name="data">The value of this tag.</param>
         public CompoundTag(Dictionary<string, ITag> data) {
             Data = new Dictionary<string, ITag>(data);
         }
 
+        /// <summary>
+        /// Gets or sets the tag associated with the specified name.
+        /// </summary>
+        /// <param name="key">The name of the tag to get or set.</param>
+        /// <returns>The tag accociated with the specified name.</returns>
         public ITag this[string key] {
             get { return Data[key]; }
             set { Data[key] = value; }
@@ -52,6 +70,11 @@ namespace ErrorCraft.Nbt.Tags {
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Adds the specified name and tag to the compound.
+        /// </summary>
+        /// <param name="key">The name of the tag to add.</param>
+        /// <param name="value">The value to add.</param>
         public void Add(string key, ITag value) {
             Data.Add(key, value);
         }
