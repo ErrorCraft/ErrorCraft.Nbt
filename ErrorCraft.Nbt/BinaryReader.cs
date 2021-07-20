7062203e1450789726937bb92c041263bbf4a4df
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-07-20 23:26:31 +0200

    Document BinaryReader with XML comments

--- ErrorCraft.Nbt/BinaryReader.cs
+++ ErrorCraft.Nbt/BinaryReader.cs
@@ -4,42 +4,77 @@ using System.IO;
 using System.Text;
 
 namespace ErrorCraft.Nbt {
+    /// <summary>
+    /// A class for reading binary data from a stream.
+    /// </summary>
     public class BinaryReader : IDisposable {
         private readonly Encoding Encoding = new ModifiedUTF8Encoding();
         private readonly Stream Stream;
         private readonly byte[] Buffer = new byte[sizeof(long)];
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="BinaryReader"/> class with the specified stream.
+        /// </summary>
+        /// <param name="stream">The input stream.</param>
         public BinaryReader(Stream stream) {
             Stream = stream;
         }
 
+        /// <summary>
+        /// Reads the next signed byte from the stream.
+        /// </summary>
+        /// <returns>The next signed byte read from the stream.</returns>
         public sbyte ReadByte() {
             FillBuffer(1);
             return (sbyte)Buffer[0];
         }
 
+        /// <summary>
+        /// Reads the next unsigned byte from the stream.
+        /// </summary>
+        /// <returns>The next unsigned byte read from the stream.</returns>
         public byte ReadUnsignedByte() {
             return (byte)ReadByte();
         }
 
+        /// <summary>
+        /// Reads the next signed 16-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next signed 16-bit integer read from the stream.</returns>
         public short ReadShort() {
             FillBuffer(2);
             return (short)(Buffer[0] << 8 | Buffer[1]);
         }
 
+        /// <summary>
+        /// Reads the next unsigned 16-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next unsigned 16-bit integer read from the stream.</returns>
         public ushort ReadUnsignedShort() {
             return (ushort)ReadShort();
         }
 
+        /// <summary>
+        /// Reads the next signed 32-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next signed 32-bit integer read from the stream.</returns>
         public int ReadInt() {
             FillBuffer(4);
             return Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3];
         }
 
+        /// <summary>
+        /// Reads the next unsigned 32-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next unsigned 32-bit integer read from the stream.</returns>
         public uint ReadUnsignedInt() {
             return (uint)ReadInt();
         }
 
+        /// <summary>
+        /// Reads the next signed 64-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next signed 64-bit integer read from the stream.</returns>
         public long ReadLong() {
             FillBuffer(8);
             uint upper = (uint)(Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3]);
@@ -47,26 +82,46 @@ namespace ErrorCraft.Nbt {
             return (long)((ulong)upper << 32 | lower);
         }
 
+        /// <summary>
+        /// Reads the next unsigned 64-bit integer from the stream.
+        /// </summary>
+        /// <returns>The next unsigned 64-bit integer read from the stream.</returns>
         public ulong ReadUnsignedLong() {
             return (ulong)ReadLong();
         }
 
+        /// <summary>
+        /// Reads the next single-precision floating point number from the stream.
+        /// </summary>
+        /// <returns>The next single-precision floating point number read from the stream.</returns>
         public unsafe float ReadFloat() {
             int i = ReadInt();
             return *(float*)&i;
         }
 
+        /// <summary>
+        /// Reads the next double-precision floating point number from the stream.
+        /// </summary>
+        /// <returns>The next double-precision floating point number read from the stream.</returns>
         public unsafe double ReadDouble() {
             long l = ReadLong();
             return *(double*)&l;
         }
 
+        /// <summary>
+        /// Reads the next length-prefixed string from the stream using the <see cref="ModifiedUTF8Encoding"/> encoding.
+        /// </summary>
+        /// <returns>The next string read from the stream.</returns>
         public string ReadString() {
             int length = ReadUnsignedShort();
             byte[] bytes = ReadBytes(length);
             return Encoding.GetString(bytes);
         }
 
+        /// <summary>
+        /// Reads the next <see cref="TagType"/> from the stream.
+        /// </summary>
+        /// <returns>The next <see cref="TagType"/> read from the stream.</returns>
         public TagType ReadTagType() {
             return (TagType)ReadByte();
         }
